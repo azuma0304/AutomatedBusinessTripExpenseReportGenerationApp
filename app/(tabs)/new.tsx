0c9ffb8f2dd57451aa946eadc1083bb934744069
azuma0304 <azuma2027work@gmail.com>
2025-09-22 18:41:52 +0900

    ReactNativeElements導入 ※RNEがreact-native-safe-area-context@3.xの要求に対して無理やり4.xの環境で動かしているので不安要素あり

--- app/(tabs)/new.tsx
+++ app/(tabs)/new.tsx
@@ -2,19 +2,23 @@
 import { ThemedText } from "../../components/themed-text";
 import { ThemedView } from "../../components/themed-view";
 import { StyleSheet } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Button, Text, Card } from "@rneui/themed";
 
 export default function NewScreen() {
   return (
-    <ThemedView style={styles.container}>
-      <ThemedText type="title">New</ThemedText>
-    </ThemedView>
+    <SafeAreaProvider>
+      <Card>
+        <Card.Title>RNE お試しカード</Card.Title>
+        <Card.Divider />
+        <Text style={{ marginBottom: 10 }}>
+          React Native Elements (RNE) のコンポーネントを使ったサンプルです。
+        </Text>
+        <Button
+          title="押してみてね"
+          onPress={() => alert("RNEボタンが押されました")}
+        />
+      </Card>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
